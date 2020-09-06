849ab6c28fbd414fb2f2331cf38e4a60680b9508
sam <samirkumarpadhi@gmail.com>
2020-09-06 13:37:43 +0530

    Message

--- Algo/src/main/java/com/go/array/PrimeNumber.java
+++ Algo/src/main/java/com/go/array/PrimeNumber.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-public class PriveSieve {
+public class PrimeNumber {
 
     // @include
     // Given n, return all primes up to and including n.
